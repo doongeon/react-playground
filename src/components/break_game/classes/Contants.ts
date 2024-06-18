8aed37a632a9d61c4b0a75db340d0cff82d9423d
donggeon Na <ndmb2012@gmail.com>
2024-06-18 23:14:45 +0900

    create: game global variables

---
+++ src/components/break_game/classes/Contants.ts
@@ -0,0 +1,6 @@
+export const PADDLE_SPEED = 7;
+export const BALL_SPEED = 7;
+export const CANVAS_WIDTH = 600;
+export const CANVAS_HEIGHT = 800;
+export const BRICKS_PER_ROW = 25;
+export const FINAL_ROUND = 3;
