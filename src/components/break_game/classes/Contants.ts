0502630a064a7330f34cf1ece4b1d6528568f88d
donggeon Na <ndmb2012@gmail.com>
2024-06-19 22:23:23 +0900

    add: brick per column

--- src/components/break_game/classes/Contants.ts
+++ src/components/break_game/classes/Contants.ts
@@ -3,4 +3,5 @@ export const BALL_SPEED = 7;
 export const CANVAS_WIDTH = 600;
 export const CANVAS_HEIGHT = 800;
 export const BRICKS_PER_ROW = 25;
+export const BRICKS_PER_COLUMN = 15;
 export const FINAL_ROUND = 3;
