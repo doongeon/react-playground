e2148c3feb6933a9c01515e1f4d96f8ef3fb130b
donggeon Na <ndmb2012@gmail.com>
2024-06-20 18:12:31 +0900

    create: game status

---
+++ src/components/break_game/components/status.tsx
@@ -0,0 +1,11 @@
+export default function Status() {
+  return (
+    <div className="game-console">
+      <div className="game-status">
+        <div id="life" />
+        <div id="round" />
+        <div id="bricks" />
+      </div>
+    </div>
+  );
+}
