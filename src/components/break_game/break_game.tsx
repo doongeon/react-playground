db8a84922e8f2358e2792f9ef12d5321210d89fc
donggeon Na <ndmb2012@gmail.com>
2024-06-20 20:21:23 +0900

    refactor: break game component

--- src/components/break_game/break_game.tsx
+++ src/components/break_game/break_game.tsx
@@ -1,77 +1,40 @@
-import { useEffect } from "react";
 import "./break_game.css";
-import { CANVAS_HEIGHT, CANVAS_WIDTH } from "./classes/Contants";
-import { Game } from "./classes/Game";
+import "./styles/star.css";
+import "./styles/game_controller.css";
+import {
+  Audio,
+  Canvas,
+  Modal,
+  PadBottom,
+  ResumeBtn,
+  Stars,
+  StartBtn,
+  Status,
+} from "./components/index";
+import useBreakGame from "./hooks/useBreakGame";
+import PauseBtn from "./components/pause_btn";
 
 export default function BreakGame() {
-  useEffect(() => {
-    const canvas = document.getElementById(
-      "break-brick-gui"
-    ) as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d");
-    const game = new Game(ctx!);
-    game.run();
-  });
+  const { gameStart, pause, canvasRef, gameRef } = useBreakGame();
 
   return (
     <div className="game-section">
-      <div className="game-machine-margin"></div>
-      <div className="game-machine-shadow"></div>
-      <div className="game-machine">
+      <div className="game-pad">
+        <Audio audioUrl="audios/megalovania.mp3" />
         <div className="canvas-container">
-          <div className="game-console">
-            <div className="game-status">
-              <div id="life"></div>
-              <div id="round"></div>
-              <div id="bricks"></div>
-            </div>
-          </div>
-          <canvas
-            id="break-brick-gui"
-            className="display"
-            width={CANVAS_WIDTH}
-            height={CANVAS_HEIGHT}
-          />
-          {[...Array(100)].map((value) => {
-            return (
-              <div
-                key={value}
-                className="star"
-                style={{
-                  top: `${Math.random() * 100}%`,
-                  left: `${Math.random() * 100}%`,
-                  animationDelay: `${Math.random() * 1.5}s`,
-                }}
-              />
-            );
-          })}
+          {!gameStart && (
+            <Modal
+              button={<StartBtn canvasRef={canvasRef} gameRef={gameRef} />}
+            />
+          )}
+
+          {pause && <Modal button={<ResumeBtn gameRef={gameRef} />} />}
+          <Status />
+          <Canvas ref={canvasRef} />
+          <Stars />
         </div>
-        <div className="buttons-wrap">
-          <div className="arrow-btn">
-            <div className="arrow-btn-item"></div>
-            <div className="arrow-btn-item"></div>
-            <div className="arrow-btn-item"></div>
-            <div className="arrow-btn-item"></div>
-            <div className="arrow-btn-item"></div>
-            <div className="arrow-btn-item"></div>
-            <div className="arrow-btn-item"></div>
-            <div className="arrow-btn-item"></div>
-            <div className="arrow-btn-item"></div>
-          </div>
-          <div className="circle-btn">
-            <div className="circle-btn-item"></div>
-            <div className="circle-btn-item"></div>
-            <div className="circle-btn-item"></div>
-            <div className="circle-btn-item"></div>
-          </div>
-        </div>
+        <PadBottom button={<PauseBtn gameRef={gameRef} />} />
       </div>
-      <div id="game-modal">
-        <div id="game-status"></div>
-        <button id="game-start-btn">game start!</button>
-        <button id="game-restart-btn">restart</button>
-      </div>
-      <div className="game-machine-margin"></div>
     </div>
   );
 }
