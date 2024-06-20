b6b90129a4a2d4cbf91ce246934eb8e338984ccd
donggeon Na <ndmb2012@gmail.com>
2024-06-20 15:42:27 +0900

    update: as browser router

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,10 +1,20 @@
-import "./Nav.css"
+import { Link } from "react-router-dom";
+import "./Nav.css";
 
 export default function Nav() {
   return (
-    <ul className="nav">
-      <li className="nav__btn">홈</li>
-      <li className="nav__btn">소개</li>
-    </ul>
+    <>
+      <ul className="nav">
+        <Link to={"/"}>
+          <li className="nav__btn">홈</li>
+        </Link>
+        <Link to={"/about-me"}>
+          <li className="nav__btn">소개</li>
+        </Link>
+        <Link to={"/games"}>
+          <li className="nav__btn">게임</li>
+        </Link>
+      </ul>
+    </>
   );
 }
