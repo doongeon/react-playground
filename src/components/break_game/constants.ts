a30cbe1afd0c50bdf4b034499ebea8dfa89fbb80
donggeon Na <ndmb2012@gmail.com>
2024-06-20 18:17:01 +0900

    create: break game components constants

---
+++ src/components/break_game/constants.ts
@@ -0,0 +1,5 @@
+export const INITIAL_MESSAGE = "break brick";
+export const WIN_MESSAGE = "win!";
+export const WIN_BUTTON_MESSAGE = "again?";
+export const LOSE_MESSAGE = "lose...";
+export const LOSE_BUTTON_MESSAGE = "restart";
