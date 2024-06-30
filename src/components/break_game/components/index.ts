b5eec900c53b1015f780e7d25f7f16ebd69d0723
donggeon Na <ndmb2012@gmail.com>
2024-06-30 09:15:53 +0900

    remove: break out game component

--- src/components/break_game/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Stars } from "./stars";
-export { default as Status } from "./status";
-export { default as Modal } from "./modal";
-export { default as Audio } from "./audio";
-export { default as Canvas } from "./canvas";
-export { default as StartBtn } from "./start_btn";
-export { default as ResumeBtn } from "./resume_btn";
-export { default as PadBottom } from "./pad_bottom";
--- src/components/break_game/constants.ts
+++
@@ -1,5 +0,0 @@
-export const INITIAL_MESSAGE = "break brick";
-export const WIN_MESSAGE = "win!";
-export const WIN_BUTTON_MESSAGE = "again?";
-export const LOSE_MESSAGE = "lose...";
-export const LOSE_BUTTON_MESSAGE = "restart";
