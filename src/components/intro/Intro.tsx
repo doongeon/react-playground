d3835afdef30e350888330a1861c54efdfaf6f57
donggeon Na <ndmb2012@gmail.com>
2024-07-05 10:56:28 +0900

    feat: add visually tracking card on intro section

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -1,25 +1,41 @@
-import "./Intro.css";
+import "./intro.css";
+import useTrackingCard from "./hooks/useTrackingCard";
 
-export default function Intro() {
+function Intro() {
+  const { cardRef, onMouseEnter, onMouseLeave } = useTrackingCard();
+
   return (
-    <div className="intro-section-container">
-      <section className="intro-section">
-        <p className="intro-section__text-container">
+    <section
+      className="intro-section"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <div className="intro-section__text-position-modifier">
+        <div className="intro-section__text-container">
           <h2 className="intro-section__title">이끌림</h2>
           <span>누군가 강요하지 않아도</span>
           <span>우리는 항상 무엇인가에 이끌립니다.</span>
           <span>무엇이 우리를 끌어당겼을까요?</span>
+        </div>
+        <div ref={cardRef} className="tracking-card">
+          <pre>
+            <span className="tracking-card__red">int</span>{" "}
+            <span className="tracking-card__blue">main</span>(
+            <span className="tracking-card__red">string</span>[]{" "}
+            <span className="tracking-card__yellow">args</span>) {"{\n"}{" "}
+            <span className="tracking-card__blue">printf</span>(
+            <span className="tracking-card__purple">"hello world!"</span>);
+            {"\n}"}
+          </pre>
+        </div>
+        <div className="intro-section__text-container">
           <span>상상하지 못했던,</span>
           <span>새롭고 즐거운 경험은 아니였을까요?</span>
           <span>우리는 즐거운 경험을 만들어 갑니다.</span>
-        </p>
-        <div className="intro-section__image-container">
-          <img
-            className="intro-section__image"
-            src="https://c10.patreonusercontent.com/4/patreon-media/p/campaign/9987336/f9daa0dce6fe4f4fbb21b5d20267ce1e/eyJ3IjoxMjAwLCJ3ZSI6MX0%3D/6.png?token-time=1720051200&token-hash=m6_dVeaErxod3TcSBJUCtZL0knyFhFyoPzwYEIjRqlM%3D"
-          />
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 }
+
+export default Intro;
