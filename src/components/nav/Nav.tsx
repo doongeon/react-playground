6763ea772f62eee07ef94a3e85f790f26d2b0afb
donggeon Na <ndmb2012@gmail.com>
2024-07-05 13:09:02 +0900

    style: color nav button

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -5,14 +5,11 @@ export default function Nav() {
   return (
     <>
       <ul className="nav">
-        <Link to={"/"}>
-          <li className="nav__btn">홈</li>
+        <Link to={"/"} className="nav__btn">
+          <li>홈</li>
         </Link>
-        {/* <Link to={"/about-me"}>
-          <li className="nav__btn">소개</li>
-        </Link> */}
-        <Link to={"/games"}>
-          <li className="nav__btn">게임</li>
+        <Link to={"/games"} className="nav__btn">
+          <li>게임</li>
         </Link>
       </ul>
     </>
