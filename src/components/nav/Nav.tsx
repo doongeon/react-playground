9124b36d3315ee7120394ddab17cb005b84acf0a
donggeon Na <ndmb2012@gmail.com>
2024-07-05 11:12:41 +0900

    update: remove "about me" link on navigation

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -8,9 +8,9 @@ export default function Nav() {
         <Link to={"/"}>
           <li className="nav__btn">홈</li>
         </Link>
-        <Link to={"/about-me"}>
+        {/* <Link to={"/about-me"}>
           <li className="nav__btn">소개</li>
-        </Link>
+        </Link> */}
         <Link to={"/games"}>
           <li className="nav__btn">게임</li>
         </Link>
