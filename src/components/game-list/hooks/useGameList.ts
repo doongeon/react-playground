ea91ac641c8d10b05c1e9f6107941ed9004d6787
donggeon Na <ndmb2012@gmail.com>
2024-07-06 10:11:11 +0900

    refactor: games section

---
+++ src/components/game-list/hooks/useGameList.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { gamesAtom } from "../../../utils/games-atom";
+
+export default function useGameList() {
+  const [isLoading, setLoading] = useState(true);
+  const [games, setGames] = useRecoilState(gamesAtom);
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      try {
+        const response = await fetch(
+          "https://f5j1rhfaph.execute-api.ap-northeast-2.amazonaws.com/api/games"
+        );
+        if (!response.ok) throw new Error("fail to fetch games");
+
+        const json = await response.json();
+        setGames(json);
+      } catch (error) {
+        console.error("Error fetching games: ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (games.length === 0) {
+      fetchGames();
+    } else {
+      setLoading(false);
+    }
+  }, [setGames, games.length]);
+
+  return { isLoading, games };
+}
