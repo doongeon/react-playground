7a0695ab29657a570a716d8a64e30f3355b0ac51
donggeon Na <ndmb2012@gmail.com>
2024-07-21 15:08:50 +0900

    style: game iframe border

--- src/components/game-list/hooks/useGameList.ts
+++ src/components/game-list/hooks/useGameList.ts
@@ -3,32 +3,33 @@ import { useRecoilState } from "recoil";
 import { gamesAtom } from "../../../utils/games-atom";
 
 export default function useGameList() {
-  const [isLoading, setLoading] = useState(true);
-  const [games, setGames] = useRecoilState(gamesAtom);
+    const [isLoading, setLoading] = useState(true);
+    const [games, setGames] = useRecoilState(gamesAtom);
 
-  useEffect(() => {
-    const fetchGames = async () => {
-      try {
-        const response = await fetch(
-          "https://f5j1rhfaph.execute-api.ap-northeast-2.amazonaws.com/api/games"
-        );
-        if (!response.ok) throw new Error("fail to fetch games");
+    useEffect(() => {
+        const fetchGames = async () => {
+            try {
+                const response = await fetch(
+                    "https://f5j1rhfaph.execute-api.ap-northeast-2.amazonaws.com/api/games"
+                );
 
-        const json = await response.json();
-        setGames(json);
-      } catch (error) {
-        console.error("Error fetching games: ", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+                if (!response.ok) throw new Error("fail to fetch games");
 
-    if (games.length === 0) {
-      fetchGames();
-    } else {
-      setLoading(false);
-    }
-  }, [setGames, games.length]);
+                const json = await response.json();
+                setGames(json);
+            } catch (error) {
+                console.error("Error fetching games: ", error);
+            } finally {
+                setLoading(false);
+            }
+        };
 
-  return { isLoading, games };
+        if (games.length === 0) {
+            fetchGames();
+        } else {
+            setLoading(false);
+        }
+    }, [setGames, games.length]);
+
+    return { isLoading, games };
 }
--- src/components/game-player/game-player.tsx
+++ src/components/game-player/game-player.tsx
@@ -3,65 +3,70 @@ import "./game-player.css";
 import { useEffect, useRef, useState } from "react";
 
 interface TGameData {
-  gameId: string;
-  title: string;
-  discription: string;
-  thumbnail: string;
-  url: string;
-  gitRepo: string;
+    gameId: string;
+    title: string;
+    discription: string;
+    thumbnail: string;
+    url: string;
+    gitRepo: string;
 }
 
 export default function GamePlayer() {
-  const { gameId } = useParams();
-  const iframeRef = useRef<HTMLIFrameElement | null>(null);
-  const [isLoading, setLoading] = useState(true);
-  const [game, setGame] = useState<TGameData | undefined>();
-  const [isError, setError] = useState(false);
+    const { gameId } = useParams();
+    const iframeRef = useRef<HTMLIFrameElement | null>(null);
+    const [isLoading, setLoading] = useState(true);
+    const [game, setGame] = useState<TGameData | undefined>();
+    const [isError, setError] = useState(false);
 
-  useEffect(() => {
-    const fetchGame = async () => {
-      try {
-        const response = await fetch(
-          `https://f5j1rhfaph.execute-api.ap-northeast-2.amazonaws.com/api/games/${gameId}`
-        );
-        if (!response.ok) throw new Error("fail to fetch games");
+    useEffect(() => {
+        const fetchGame = async () => {
+            try {
+                const response = await fetch(
+                    `https://f5j1rhfaph.execute-api.ap-northeast-2.amazonaws.com/api/games/${gameId}`
+                );
+                if (!response.ok) throw new Error("fail to fetch games");
 
-        const json = await response.json();
-        setGame(json);
-      } catch (error) {
-        console.error("Error fetching games: ", error);
-        setError(true);
-      } finally {
-        setLoading(false);
-      }
-    };
+                const json = await response.json();
+                setGame(json);
+            } catch (error) {
+                console.error("Error fetching games: ", error);
+                setError(true);
+            } finally {
+                setLoading(false);
+            }
+        };
 
-    fetchGame();
-  }, [gameId]);
+        fetchGame();
+    }, [gameId]);
 
-  return (
-    <div className="game-player">
-      {isLoading ? (
-        <div>loading...</div>
-      ) : isError ? (
-        <div>Error</div>
-      ) : (
-        <>
-          <h1 className="game-player__title">{game?.title}</h1>
-          <div className="game-player__discription">
-            <span>{game?.discription}</span>
-            <a
-              href={game?.gitRepo}
-              target="_blank"
-              className="game-player__git-anchor"
-            >
-              visit git repo
-            </a>
-          </div>
-          <iframe ref={iframeRef} src={game?.url} width="100%" height="900" />
-          <Link to={"/games"}>목록으로</Link>
-        </>
-      )}
-    </div>
-  );
+    return (
+        <div className="game-player">
+            {isLoading ? (
+                <div>loading...</div>
+            ) : isError ? (
+                <div>Error</div>
+            ) : (
+                <>
+                    <h1 className="game-player__title">{game?.title}</h1>
+                    <div className="game-player__discription">
+                        <span>{game?.discription}</span>
+                        <a
+                            href={game?.gitRepo}
+                            target="_blank"
+                            className="game-player__git-anchor"
+                        >
+                            visit git repo
+                        </a>
+                    </div>
+                    <iframe
+                        ref={iframeRef}
+                        src={game?.url}
+                        width="100%"
+                        height="900"
+                    />
+                    <Link to={"/games"}>목록으로</Link>
+                </>
+            )}
+        </div>
+    );
 }
