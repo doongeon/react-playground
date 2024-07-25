758e1aa7fe2fc2f225b479a73e814ec1c3535924
donggeon Na <ndmb2012@gmail.com>
2024-07-25 12:33:16 +0900

    refactor: subtract variable spreadBoardRef

--- src/common/components/spread_contents/spread_board.tsx
+++ src/common/components/spread_contents/spread_board.tsx
@@ -2,25 +2,27 @@ import "./styles/spread_board.css";
 import useSpreadBoard from "./hooks/useSpreadBoard";
 import RestartBtn from "./components/restart_btn";
 import SpreadItem from "./components/spread_Item";
+import { useRef } from "react";
 
 export default function SpreadBoard() {
-  const { spreadBoardRef } = useSpreadBoard();
+    const spreadBoardRef = useRef<HTMLDivElement>(null);
+    useSpreadBoard({ spreadBoardRef });
 
-  return (
-    <div className="spread-board-container">
-      <div ref={spreadBoardRef} className="spread-board">
-        <div className="spread-board__center"></div>
-        {[...Array(6)].map((__, index) => (
-          <SpreadItem
-            key={index}
-            imageUrl={`/images/content_${index + 1}.png`}
-            index={index}
-          />
-        ))}
-        <div>
-          <RestartBtn />
+    return (
+        <div className="spread-board-container">
+            <div ref={spreadBoardRef} className="spread-board">
+                <div className="spread-board__center"></div>
+                {[...Array(6)].map((__, index) => (
+                    <SpreadItem
+                        key={index}
+                        imageUrl={`/images/content_${index + 1}.png`}
+                        index={index}
+                    />
+                ))}
+                <div>
+                    <RestartBtn />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
