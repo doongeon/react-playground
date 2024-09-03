e7ec99860c8d13c1497daeb45ba3e0000d9994de
donggeon Na <ndmb2012@gmail.com>
2024-09-03 15:23:00 +0900

    feat: add footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import "./footer.css";
+
+export default function Footer() {
+    return (
+        <section className="footer">
+            <div className="footer-img"></div>
+        </section>
+    );
+}
