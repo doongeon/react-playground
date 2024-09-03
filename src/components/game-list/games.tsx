b76447f786735717984e78d1027b178f79344e2b
donggeon Na <ndmb2012@gmail.com>
2024-09-03 15:23:43 +0900

    feat: add notice

--- src/components/game-list/games.tsx
+++ src/components/game-list/games.tsx
@@ -1,27 +1,30 @@
 import "./styles/games.css";
 import GameCard from "./components/game-card";
-import GameCardSkeleton from "./components/game-card-skeleton";
-import useGameList from "./hooks/useGameList";
+import useGameList from "../../common/hooks/useGameList";
 
 export default function GameList() {
-  const { isLoading, games } = useGameList();
+    const { isLoading, games } = useGameList();
 
-  return (
-    <section className="game-list">
-      {isLoading ? (
-        <GameCardSkeleton />
-      ) : (
-        games.map((game) => {
-          return (
-            <GameCard
-              key={game.gameId}
-              id={game.gameId}
-              title={game.title}
-              thumbnail={game.thumbnail}
-            />
-          );
-        })
-      )}
-    </section>
-  );
+    return (
+        <section className="game-section">
+            <div className="notice">
+                <span>Sorry... 😰</span>
+                <span>모바일에서는 게임을 즐기실 수 없습니다.</span>
+            </div>
+            <div className="game-list">
+                {isLoading
+                    ? "loading"
+                    : games.map((game) => {
+                          return (
+                              <GameCard
+                                  key={game.gameId}
+                                  id={game.gameId}
+                                  title={game.title}
+                                  thumbnail={game.thumbnail}
+                              />
+                          );
+                      })}
+            </div>
+        </section>
+    );
 }
