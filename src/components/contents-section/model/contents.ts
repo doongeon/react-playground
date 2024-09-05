9ab44580c8477e6967b38632427c5eff09c07473
donggeon Na <ndmb2012@gmail.com>
2024-09-05 15:18:56 +0900

    feat: add content section model

---
+++ src/components/contents-section/model/contents.ts
@@ -0,0 +1,24 @@
+interface content {
+    img: string;
+}
+
+export const contents: content[] = [
+    {
+        img: "images/spreadBorad_contents/bg3.png",
+    },
+    {
+        img: "images/spreadBorad_contents/snc.png",
+    },
+    {
+        img: "images/spreadBorad_contents/tetris.png",
+    },
+    {
+        img: "images/spreadBorad_contents/breakout.png",
+    },
+    {
+        img: "images/spreadBorad_contents/animation.png",
+    },
+    {
+        img: "images/spreadBorad_contents/tetris.png",
+    },
+];
