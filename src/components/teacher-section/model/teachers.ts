28cae57889f27c2221c912a2732d06d968b38654
donggeon Na <ndmb2012@gmail.com>
2024-09-05 15:24:42 +0900

    feat: add teacher model

---
+++ src/components/teacher-section/model/teachers.ts
@@ -0,0 +1,27 @@
+interface t_teacher {
+    name: string;
+    img: string;
+}
+
+export const teachers: t_teacher[] = [
+    {
+        name: "Nicolas",
+        img: "https://d1telmomo28umc.cloudfront.net/media/public/avatars/serranoarevalo-1679363190.jpg",
+    },
+    {
+        name: "Kevin Powell",
+        img: "https://yt3.googleusercontent.com/ytc/AIdro_mQczyuvnXgEq8fApoOXpG2Yw_JKYqRA7kVOhNFuHLz9Vc=s176-c-k-c0x00ffffff-no-rj",
+    },
+    {
+        name: "Paul Hegarty",
+        img: "https://yt3.googleusercontent.com/6LYCodB2l4h3oSzjFGDFxPJtHiLAbM9rsmzam9ma7EhdOCIdXXCxpgSXamWg_W7GqLVj-U-L=s160-c-k-c0x00ffffff-no-rj",
+    },
+    {
+        name: "생활 코딩",
+        img: "https://yt3.googleusercontent.com/ytc/AIdro_lbqqh0NSFeq6P1F-6qRycC37MtiGyMjZFVsZ4J-c6C474=s160-c-k-c0x00ffffff-no-rj",
+    },
+    {
+        name: "Dave Gray",
+        img: "https://yt3.googleusercontent.com/0eIqxAMRNI9gnG0HXSBnpFCmV5E5UjsxzZ3HtvG9Q9PD8QxnZRE9QWvuoiDS8HFEgeFd1Hc4=s160-c-k-c0x00ffffff-no-rj",
+    },
+];
