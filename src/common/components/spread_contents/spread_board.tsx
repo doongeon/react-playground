4b50ae397a6cb35d6ec4905c18e614ae21a1420a
donggeon Na <ndmb2012@gmail.com>
2024-10-24 22:45:08 +0900

    feat: add link to spread board item

--- src/common/components/spread_contents/spread_board.tsx
+++ src/common/components/spread_contents/spread_board.tsx
@@ -2,9 +2,11 @@ import "./styles/spread_board.css";
 import useSpreadBoard from "./hooks/useSpreadBoard";
 import SpreadItem from "./components/spread_Item";
 import { useRef } from "react";
+import { Link } from "react-router-dom";
 
 export interface t_spreadItem {
     img: string;
+    gameId?: string;
 }
 
 interface SpreadBoardProps {
@@ -19,13 +21,22 @@ export default function SpreadBoard({ spreadItems }: SpreadBoardProps) {
         <div className="spread-board-container">
             <div ref={spreadBoardRef} className="spread-board">
                 <div className="spread-board__center">IDEA</div>
-                {spreadItems.map((spreadItem, index) => (
-                    <SpreadItem
-                        key={index}
-                        imageUrl={spreadItem.img}
-                        index={index}
-                    />
-                ))}
+                {spreadItems.map((spreadItem, index) =>
+                    spreadItem.gameId ? (
+                        <Link key={index} to={`/game/${spreadItem.gameId}`}>
+                            <SpreadItem
+                                imageUrl={spreadItem.img}
+                                index={index}
+                            />
+                        </Link>
+                    ) : (
+                        <SpreadItem
+                            key={index}
+                            imageUrl={spreadItem.img}
+                            index={index}
+                        />
+                    )
+                )}
             </div>
         </div>
     );
--- src/components/contents-section/model/contents.ts
+++ src/components/contents-section/model/contents.ts
@@ -1,21 +1,27 @@
 interface content {
     img: string;
+    gameId?: string;
+    url?: string;
 }
 
 export const contents: content[] = [
     {
         img: "images/spreadBorad_contents/bg3.png",
+        url: "",
     },
     {
         img: "images/spreadBorad_contents/snc.png",
     },
     {
         img: "images/spreadBorad_contents/tetris.png",
+        gameId: "2",
     },
     {
         img: "images/spreadBorad_contents/breakout.png",
+        gameId: "1",
     },
     {
         img: "images/spreadBorad_contents/animation.png",
+        gameId: "3",
     },
 ];
